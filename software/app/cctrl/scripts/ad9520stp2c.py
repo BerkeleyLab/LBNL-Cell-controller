112b1e50cbbc7b0b8342011433f85f97140eabac
Lucas Russo <lerwys@gmail.com>
2022-07-19 16:54:47 -0700

    software/app: fix syntax/idiomatic errors

--- software/app/cctrl/scripts/ad9520stp2c.py
+++ software/app/cctrl/scripts/ad9520stp2c.py
@@ -17,16 +17,16 @@ for line in sys.stdin:
             regList.append([reg, val])
     if (re.match(r'^"Other Settings', line)):
         show = True
-        print "/*"
+        print("/*")
     if (re.match(r'^""', line)) and show:
         show = False
-        print " */"
+        print(" */")
     if show:
-        print " * %s" % (re.sub('"', '', line.strip()))
+        print(" * %s" % (re.sub('"', '', line.strip())))
 if show:
     show = False
-    print " */"
-print "static const uint16_t XXXX[] = {"
+    print(" */")
+print("static const uint16_t XXXX[] = {")
 for l in regList:
-    print "    0x%03X, 0x%02X," % (l[0], l[1])
-print "};"
+    print("    0x%03X, 0x%02X," % (l[0], l[1]))
+print("};")
--- software/app/cctrl/scripts/createPLLtables.sh
+++ software/app/cctrl/scripts/createPLLtables.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
@@ -6,9 +6,9 @@ STP_FILES=$@
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
-idx = 0
+idx=0
 for f in $STP_FILES
 do
     python3 ${SCRIPTPATH}/ad9520stp2c.py < $f | sed -e "s/XXXX/pt${idx}table/g"
-    idx = $((idx + 1))
+    idx=$((idx + 1))
 done
