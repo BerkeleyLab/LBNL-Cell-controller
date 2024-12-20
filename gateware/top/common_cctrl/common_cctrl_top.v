2a256a5b67e5e1c081de07cd99b7928015e23db4
Lucas Russo <lerwys@gmail.com>
2024-12-20 14:55:12 -0800

    top/common_cctrl: fix QSFP mapping comment

--- gateware/top/common_cctrl/common_cctrl_top.v
+++ gateware/top/common_cctrl/common_cctrl_top.v
@@ -34,28 +34,10 @@ module common_cctrl_top #(
   output wire        RGMII_TX_CTRL,
   output wire  [3:0] RGMII_TXD,
 
-/*
-  Transceiver Assignments (Kintex-7):
-  -----------------------------------
-    This is copied directly from
-      https://controls.als.lbl.gov/alscg/beampositionmonitor/BPM_CC/Documents/HardwareNotes.html
-
-    RX N/P  TX N/P  Tile  MGT           Fiber Pair  QSFP (BMB7) QSFP (Marble)  Desc.
-    --------------------------------------------------------------------------------
-    C3/C4   B1/B2   X0Y6  MGT2 Bank 116 1:12        1-0         1-1             EVR
-    B5/B6   A3/A4   X0Y7  MGT3 Bank 116 2:11        1-1         1-3             BPM CCW
-    E3/E4   D1/D2   X0Y5  MGT1 Bank 116 3:10        1-2         1-0             BPM CW
-    G3/G4   F1/F2   X0Y4  MGT0 Bank 116 4:9         1-3         1-2             (Unused)
-    L3/L4   K1/K2   X0Y2  MGT2 Bank 115 1:12        2-0         2-1             Cell CCW
-    J3/J4   H1/H2   X0Y3  MGT3 Bank 115 2:11        2-1         2-3             Cell CW
-    N3/N4   M1/M2   X0Y1  MGT1 Bank 115 3:10        2-2         2-0             FOFB power supply chain head (Tx)
-    R3/R4   P1/P2   X0Y0  MGT0 Bank 115 4:9         2-3         2-2             FOFB power supply chain tail (Rx)
-*/
-
-  input  wire  [3:0] QSFP1_RX_N, QSFP1_RX_P, // [0]->EVR;     [1]->BPM_CCW_GT_RX_rxn;   [2]->BPM_CW_GT_RX_rxn;    [3]->BPM_TEST_RX
-  output wire  [3:0] QSFP1_TX_N, QSFP1_TX_P, // [0]->EVR;     [1]->BPM_CCW;             [2]->BPM_CW;              [3]->BPM_TEST_TX
-  input  wire  [3:0] QSFP2_RX_N, QSFP2_RX_P, // [0]->CELL_CCW_GT_RX_rxn; [1]->CELL_CW_GT_RX_rxn; [2]->fofb(psTx); [3]->fofb(psRx)
-  output wire  [3:0] QSFP2_TX_N, QSFP2_TX_P, // [0]->CELL_CCW_GT_TX_txn; [1]->CELL_CW_GT_TX_txn; [2]->fofb(psTx); [3]->fofb(psRx)
+  input  wire  [3:0] QSFP1_RX_N, QSFP1_RX_P, // [0]->EVR                ; [1]->BPM_CCW           ; [2]->BPM_CW     ; [3]->BPM_TEST_RX
+  output wire  [3:0] QSFP1_TX_N, QSFP1_TX_P, // [0]->EVR                ; [1]->BPM_CCW           ; [2]->BPM_CW     ; [3]->BPM_TEST_TX
+  input  wire  [3:0] QSFP2_RX_N, QSFP2_RX_P, // [0]->CELL_CCW           ; [1]->CELL_CW           ; [2]->fofb(psTx) ; [3]->fofb(psRx)
+  output wire  [3:0] QSFP2_TX_N, QSFP2_TX_P, // [0]->CELL_CCW_GT        ; [1]->CELL_CW           ; [2]->fofb(psTx) ; [3]->fofb(psRx)
 
   inout TWI_SDA,
   inout TWI_SCL,
