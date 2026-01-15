a48e94de37d47e9b860867d24ee28bbbbccdcbbc
Lucas Russo <lerwys@gmail.com>
2026-01-15 11:36:07 -0800

    software: fix writing to a WP sector without checking first

--- software/src/util.h
+++ software/src/util.h
@@ -17,6 +17,17 @@
 #define printf(...) xil_printf(__VA_ARGS__)
 
 /*
+ * Read/Write registers
+ */
+#ifndef __REG_MACROS_DEFINED__
+#define __REG_MACROS_DEFINED__
+#define REG_GEN_MASK(offset, size) (((1<<(size))-1) << (offset))
+#define REG_GEN_WRITE(value, offset, size) (((value) & ((1<<(size))-1)) << (offset))
+#define REG_GEN_READ(reg, offset, size) (((reg) >> (offset)) & ((1<<(size))-1))
+#define REG_SIGN_EXTEND(value, bits) (((value) & (1<<bits) ? ~((1<<(bits))-1): 0 ) | (value))
+#endif
+
+/*
  * Diagnostics
  */
 #define DEBUGFLAG_EPICS                     0x1
