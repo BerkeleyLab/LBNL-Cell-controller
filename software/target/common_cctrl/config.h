06052729760871a5e90863098c5a020c13cb2d79
Lucas Russo <lerwys@gmail.com>
2026-07-24 14:13:54 -0700

    software: add config.h file

---
+++ software/target/common_cctrl/config.h
@@ -0,0 +1,16 @@
+/*
+ * Configuration parameters shared between software and firmware
+ * The restrictions noted in gpio.h apply here, too.
+ */
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#include <assert.h>
+
+/*
+ * Number of internal fans
+ */
+#define CFG_FAN_COUNT   2
+
+#endif /* _CONFIG_H_ */
