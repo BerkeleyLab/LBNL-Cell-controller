0369b59e89c0d91b7754a36484ef1209e732f2bb
Lucas Russo <lerwys@gmail.com>
2026-07-31 12:42:42 -0700

    software/*/fofbEthernet: add routines to retrieve gpio indeces safely

--- software/src/fofbEthernet.h
+++ software/src/fofbEthernet.h
@@ -1,6 +1,10 @@
 #ifndef _FOFB_ETHERNET_H_
 #define _FOFB_ETHERNET_H_
 
+#define PS_ETH_TX_IDX           0
+#define PS_ETH_RX_IDX           1
+#define PS_ETH_NUM_DEVICES      2
+
 void fofbEthernetInit(void);
 void fofbEthernetShowStatus(void);
 uint32_t fofbEthernetGetPCSPMAstatus(void);
